ef199de63f4e77a0669fa1f06b725695c0ab6c5a
KUNPL labr3 <labr3@localhost.localdomain>
2021-04-07 11:19:31 +0900

    first commit

---
+++ allnewdecoder_fasttrig_waveform.c
@@ -0,0 +1,359 @@
+#include <iostream>
+#include "TTree.h"
+#include "TFile.h"
+#include "./include/keyb.c"
+#include "./include/gettimef.c"
+#include "./include/Pulse1024.h"
+#include "./include/DecoderCanvas.h"
+
+void waveformcopy(Float_t arrdest[1024],const Float_t arrsour[1024]){
+	for (int iwft=0; iwft<1024; iwft++){
+		arrdest[iwft]=arrsour[iwft];
+	}
+}
+
+
+void KeyboardCommand(int pqrjd_flag[5]){		
+	if(!kbhit())	return;
+	int key = getch();
+	switch(key){
+		case 'p' :						// 'p'ause
+			pqrjd_flag[0]^=1; 
+			if( pqrjd_flag[0]) printf("\nPaused. Press p again to resume\n");
+			if(!pqrjd_flag[0]) printf("resumed\n");
+			break;
+		case 'q' : 
+			pqrjd_flag[1]^=1; break;			// 'q'uit
+		case 'Q' : 
+			pqrjd_flag[1]=2; break;			// 'q'uit
+		case 'r' : 						// 'r'enew histograms
+			pqrjd_flag[2]^=1; 
+			if( pqrjd_flag[2]) printf("\nRenewing histograms is enabled.\n");
+			if(!pqrjd_flag[2]) printf("\nRenewing histograms is disabled.\n");
+			break;
+		case 'j' : 
+			pqrjd_flag[3]^=1; break;			// saving canvas to filename_canvasname.'j'pg
+		case 'd' :
+			pqrjd_flag[4]=1;
+			printf("\ndisplaying waveform\n"); break;               // pause and 'd'isplay current waveform
+		default : break;
+	}
+}
+//////////////////////main function////////////////////////////////
+
+void allnewdecoder_fasttrig_waveform(){
+	//char filename[90]="PMT_set3_12ch_Eu152_run01";
+	char filename[90]="PMT_set3_12ch_Co60_run02";
+	//char filename[90]="PMT_set1_12ch_Eu152_run01_efftest";
+	//char filename[90]="PMT_set2_12ch_Eu152_run01_efftest";
+	FILE* datafile = fopen(Form("%s.dat",filename),"rb");	// moved from latter to here
+	if(datafile==NULL){
+		fputs("File error\n",stderr);
+		exit(1);
+	}
+
+//tree
+	const UInt_t NOpGr = 2;	// number of open groups
+	const UInt_t NOpCh = 6;	// number of open channels
+	const UInt_t waveform_length = 1024;
+	const Float_t samplingrate = 2.5;  // GS/s
+	const Float_t sampletime = 1.0/samplingrate;  // ns
+	float waveformtime[waveform_length]; for (int iwft=0; iwft<waveform_length; iwft++) waveformtime[iwft]=sampletime*(float)iwft;
+	printf("samplingrate %f, sampletime %f, waveform_length %d, fullwft %f\n", samplingrate, sampletime, waveform_length, sampletime*waveform_length);
+
+	const int sample_byte = 4*(waveform_length+6);
+	fseek(datafile,0,SEEK_END);
+	ULong64_t Filesize = ftell(datafile);
+	rewind(datafile);
+	uint32_t numevent = Filesize/sample_byte;
+	uint32_t remainder = Filesize%sample_byte;
+	printf("%s.dat Filesize: %lld(Bytes), numevent: %u, Error:%u\n", filename, Filesize, numevent, remainder);
+
+	//Float_t waveformBuffer[waveform_length];
+
+	TFile* treefile = new TFile(Form("../../DecodedDataFiles/%s_waveform.root",filename),"recreate");
+	TTree* tree = new TTree("wavedata","wavedata");
+	UInt_t EventSize;
+	//UInt_t BoardID=0;
+	UInt_t NHitCh = NOpCh*NOpGr;	//number of hit channels, maximum NOpCh*NOpGr
+	UInt_t StartIndexCell[NHitCh];
+	//UInt_t Group;
+	UInt_t Group[NHitCh];
+	UInt_t Channel[NHitCh];
+	UInt_t EventNumber;
+	UInt_t TrigTimeTag;
+	Float_t waveform[NHitCh][waveform_length];
+	//Float_t waveformtime[waveform_length];
+	Float_t rPH[NHitCh];
+	Float_t QDC[NHitCh];
+	Float_t TDC[NHitCh];
+	Float_t FT_time[NOpGr];
+
+	tree->Branch("EventNumber", &EventNumber, "EventNumber/i");
+	tree->Branch("TrigTimeTag", &TrigTimeTag, "TrigTimeTag/i");
+	tree->Branch("EventSize", &EventSize, "EventSize/i");
+	//tree->Branch("BoardID", &BoardID, "BoardID/i");
+	//tree->Branch("StartIndexCell", &StartIndexCell, "StartIndexCell/i");
+	//tree->Branch("Group", &Group, "Group/i");
+	tree->Branch("NHitCh", &NHitCh, "NHitCh/i");
+	tree->Branch("StartIndexCell", StartIndexCell, "StartIndexCell[NHitCh]/i");
+	tree->Branch("Channel", Channel, "Channel[NHitCh]/i");
+	tree->Branch("Group", Group, "Group[NHitCh]/i");
+	//tree->Branch("waveform_length", &waveform_length, "waveform_length/i");
+	tree->Branch("waveform", waveform,"waveform[NHitCh][1024]/F");
+	//tree->Branch("waveform", waveform,"waveform[NHitCh][waveform_length]/F");
+//	tree->Branch("waveformtime", waveformtime,"waveformtime[waveform_length]/F");
+	tree->Branch("rPH", rPH, "rPH[NHitCh]/F");
+	tree->Branch("QDC", QDC, "QDC[NHitCh]/F");
+	tree->Branch("TDC", TDC, "TDC[NHitCh]/F");
+	tree->Branch("FT_time", FT_time, "FT_time[2]/F");
+
+
+//canvas
+	const Int_t QDCrange = 300000/2.5*samplingrate;
+	Double_t QDCcut[NOpCh][2] = {{8.40333e+04,1.35843e+05},{8.48711e+04,1.22737e+05},{8.48711e+04,1.22737e+05},{8.48711e+04,1.22737e+05},{8.48711e+04,1.22737e+05},{8.48711e+04,1.22737e+05}};
+
+	DECODER_CANVAS *canvases = new DECODER_CANVAS(NOpGr, NOpCh);
+	canvases->Construct();	
+
+	DECODER_PRIMARY_H1 *h1_primary[NOpGr];
+	for (int igr=0; igr<NOpGr; igr++){
+		h1_primary[igr] = new DECODER_PRIMARY_H1(igr, NOpCh, QDCrange, waveform_length, sampletime);
+		h1_primary[igr]->Draw(canvases);
+	}
+	//DECODER_QDC_H1_COIN *h1_QDC_ch[NOpCh];
+	//DECODER_QDC_H2 *h2_QDC_xaxis[NOpCh];
+	//DECODER_TDCDIF_H1 *h1_TDCdif[NOpCh];
+	for (int ich=0; ich<NOpCh; ich++){
+		//h1_QDC_ch[ich] = new DECODER_QDC_H1_COIN(NOpCh, ich, QDCrange);
+		//h1_QDC_ch[ich]->Draw(canvases);
+		//h2_QDC_xaxis[ich] = new DECODER_QDC_H2(NOpCh, ich, QDCrange);
+		//h2_QDC_xaxis[ich]->Draw(canvases);
+		//h1_TDCdif[ich] = new DECODER_TDCDIF_H1(NOpCh, ich, -3, 3);
+		//h1_TDCdif[ich]->Draw(canvases);
+	}
+	TCanvas *c_waveform = new TCanvas("waveform", "waveform", 800, 600);
+//	TGraph *gr_waveform;
+
+//////////////////////
+
+	uint32_t header[6];
+	int Nheader, Nwaveform;
+	uint64_t Curr_Time, Prev_Time, Elap_Time;
+	uint64_t Curr_ient, Prev_ient, Elap_ient;
+	uint64_t CurrByte, EndByte;
+	//float decoding_speed; uint64_t time_left;
+	int pqrjd_flag[5]={0,0,0,0,0};
+	int event_flag=0;
+	int fread_flag;
+	NHitCh=0;
+
+	CurrByte = ftell(datafile);
+	Nheader=(int)fread(header, 4, 6, datafile);
+	EventNumber=header[4];		//Get first event number.
+	fseek(datafile,CurrByte,SEEK_SET);	//return to begining of the file
+
+	Pulse1024* pulseBuffer;
+	Pulse1024* pulse[NOpCh*NOpGr];
+	int leakage=0;
+
+	uint64_t ient=0;
+	for (pqrjd_flag[0]=0; ient<numevent/12/2; ){
+		Curr_ient = ient;	Elap_ient = Curr_ient - Prev_ient;
+		Curr_Time = get_time();	Elap_Time = Curr_Time - Prev_Time; 
+
+		KeyboardCommand(pqrjd_flag);
+		if(pqrjd_flag[1]) event_flag=1;
+		//if(pqrjd_flag[1]) break;
+		else if(pqrjd_flag[0]){sleep(1); continue;}
+
+
+		CurrByte = ftell(datafile);
+		fseek(datafile,0,SEEK_END);
+		EndByte = ftell(datafile);
+		numevent = EndByte/sample_byte;
+		fseek(datafile,CurrByte,SEEK_SET);
+		if(EndByte-CurrByte>=sample_byte){
+			fread_flag=1;
+			Nheader=(int)fread(header, 4, 6, datafile);
+			pulseBuffer = new Pulse1024(header);	leakage++;
+			Nwaveform=(int) fread(pulseBuffer->waveform, 4, waveform_length, datafile);
+			//Nwaveform=(int) fread(waveformBuffer, 4, waveform_length, datafile);
+			//printf("EventSize %u, Group %u, StartIndexCellch %u, Channel %u, EventNumberch %u, TrigTimeTagch %u \n ", header[0],header[1],header[2],header[3],header[4],header[5]);
+			if(header[0]!=4*(Nheader+Nwaveform)){
+				printf("\nData Size Error!!  ient: %lu, header[0]: %d, Nheader+Nwaveform: %d\n", ient, header[0], Nheader+Nwaveform);
+				break;
+			}
+			if(pqrjd_flag[4]){
+				if(c_waveform==NULL) TCanvas *c_waveform = new TCanvas("waveform", "waveform", 800, 600);
+				c_waveform->cd();
+				//if(gr_waveform!=NULL) gr_waveform->~TGraph();
+				//gr_waveform = new TGraph(waveform_length, waveformtime, pulseBuffer->waveform);
+				TGraph *gr_waveform = new TGraph(waveform_length, waveformtime, pulseBuffer->waveform);
+				gr_waveform->SetName("waveform");
+				gr_waveform->SetTitle(Form("waveform %d gr%dch%d", pulseBuffer->EventNumber, pulseBuffer->Group, pulseBuffer->Channel));
+				gr_waveform->Draw("AP");
+				c_waveform->WaitPrimitive();
+
+				delete gr_waveform;
+				//gr_waveform->~TGraph();
+				pqrjd_flag[4]=0;
+			}
+			ient++;
+		}
+		else if(EndByte-CurrByte<sample_byte){
+			printf("(ONLINE)");
+			fflush(stdout);
+			fread_flag=0;
+		}
+
+		if(fread_flag==1 && pulseBuffer->EventNumber!=EventNumber && NHitCh!=0){
+			event_flag=1;
+			for (int ihit=0; ihit<NHitCh-1; ihit++){
+				if(NHitCh==1) break;
+				if(pulse[ihit]->EventNumber!=pulse[ihit+1]->EventNumber){event_flag=-1; break;}
+				if(pulse[ihit]->TrigTimeTag!=pulse[ihit+1]->TrigTimeTag){event_flag=-2; break;}
+				//if(pulse[ihit]->StartIndexCell!=pulse[ihit+1]->StartIndexCell){event_flag=-3;break;}
+			}
+			if(event_flag==-1){
+				printf("EventNumber missmatch @CurrByte %lu!\n", CurrByte, stderr);
+				return;
+			}
+			if(event_flag==-2){
+				printf("TrigTimeTag missmatch!\n", stderr);
+				return;
+			}
+			if(event_flag==-3){
+				printf("StartIndexCell missmatch!\n", stderr);
+				return;
+			}
+		}
+
+		if(event_flag==1){
+			EventNumber = pulse[0]->EventNumber; TrigTimeTag = pulse[0]->TrigTimeTag;	EventSize=pulse[0]->EventSize;
+			//EventNumber = pulse[0]->EventNumber; TrigTimeTag = pulse[0]->TrigTimeTag;	StartIndexCell=pulse[0]->StartIndexCell;	EventSize=pulse[0]->EventSize;
+			for (int i=0; i<NHitCh; i++){
+				rPH[i]=pulse[i]->rPH;
+				QDC[i]=pulse[i]->QDC;
+				TDC[i]=pulse[i]->TDC;
+				Group[i]=pulse[i]->Group;
+				Channel[i]=pulse[i]->Channel;
+				StartIndexCell[i]=pulse[i]->StartIndexCell;
+				waveformcopy(waveform[i],pulse[i]->waveform);
+				delete pulse[i]; leakage--;
+			}
+			for (int i=0; i<NHitCh; i++){
+				if(rPH[i]>3850) continue;
+				if(QDC[i]< 1000) continue;
+				h1_primary[Group[i]]->rPH[Channel[i]]->Fill(rPH[i]);
+				h1_primary[Group[i]]->QDC[Channel[i]]->Fill(QDC[i]);
+				h1_primary[Group[i]]->TDC[Channel[i]]->Fill(TDC[i]);
+			}
+/*			for (int i=0; i<NHitCh; i++){
+				if(Group[i]!=0) continue;
+				//if(rPH[i]>3850) continue;
+				for (int j=0; j<NHitCh; j++){
+					if(Group[j]!=0) continue;
+					//if(rPH[j]>3850) continue;
+					//h1_QDC_ch[Channel[i]]->coin[Channel[j]]->Fill(QDC[i]);
+					//h2_QDC_xaxis[Channel[i]]->yaxis[Channel[j]]->Fill(QDC[i],QDC[j]);
+					if(QDCcut[Channel[i]][0]-2.0*QDCcut[Channel[i]][1]<QDC[i] && QDC[i]<QDCcut[Channel[i]][0]+2.0*QDCcut[Channel[i]][1]){
+						if(QDCcut[Channel[j]][0]-2.0*QDCcut[Channel[j]][1]<QDC[j] && QDC[j]<QDCcut[Channel[j]][0]+2.0*QDCcut[Channel[j]][1]){
+							if(TDC[i]<0) continue;
+							if(TDC[j]<0) continue;
+							//h1_TDCdif[Channel[i]]->startch[Channel[j]]->Fill(TDC[i]-TDC[j]);
+						}
+					}
+				}
+			}	
+*/			tree->Fill();
+
+			NHitCh=0; FT_time[0]=0.; FT_time[1]=0.;
+			event_flag=0;
+		}
+
+		if(fread_flag && pulseBuffer->Channel<NOpCh){
+			EventNumber=pulseBuffer->EventNumber;
+			//pulseBuffer->Analyze(0,200, 200,950, 10, 0.50, samplingrate, 200,600);
+			pulseBuffer->Analyze(0,200, 200,950, 10, 0.15, samplingrate, 200,600);
+			pulse[NHitCh]=pulseBuffer;
+
+			//event_flag=0;
+			NHitCh++;
+		}
+		if(fread_flag && pulseBuffer->Channel>=NOpCh){
+			if(pulseBuffer->Channel ==8){
+				FT_time[pulseBuffer->Group]=pulseBuffer->FastTrig_Analyze(samplingrate, /*float voltage_l=*/1024., /*float voltage_t=*/2048.);
+			}
+			delete pulseBuffer;	leakage--;
+		}
+
+
+		if(pqrjd_flag[1]){
+			printf("\nquit decoding\n");
+			break;
+		}
+
+		if(Elap_Time > 1000){
+			//decoding_speed=(float)Elap_ient*1000.0f/(float)Elap_Time;
+			//time_left=(numevent-ient)/(int)decoding_speed;
+			//printf("\rdecoding... at %lu of %u (%.3f) %.0fient/s ETF %lusec leakage %d", ient, numevent, (float) ient/numevent, decoding_speed, time_left, leakage);
+			printf("\rdecoding... at %lu of %u (%.3f) %.0fient/s leakage %d", ient, numevent, (float) ient/numevent, (float)Elap_ient*1000.0f/(float)Elap_Time, leakage);
+			fflush(stdout);
+			Prev_Time = Curr_Time;
+			Prev_ient = Curr_ient;
+			if(pqrjd_flag[2] || pqrjd_flag[3]){
+				canvases->Modified_Update(0x01);
+				//canvases->Modified_Update(0x0F);
+			}
+			if(pqrjd_flag[3]){
+				canvases->Modified_Update(0x0F);
+				printf("\n");
+				canvases->Print(filename);
+				pqrjd_flag[3]=0;
+			}
+		}
+		if(!fread_flag){
+			sleep(1);
+			continue;
+		}
+	}
+
+	printf("\nReading completed.Entries: %lu, Event numbers: %u\n", ient, EventNumber);
+
+	if(pqrjd_flag[1]==2)	printf("Force Quit");
+	else	canvases->Modified_Update(0x01);
+	//else	canvases->Modified_Update(0x0F);
+
+
+/*
+	TF1 *Na662[NOpCh];
+	double fitpar[NOpCh][5] ={	{400, 84000,1000,0,0},
+					{300, 83700,1300,0,0},
+					{300, 83700,1300,0,0},
+					{300, 83700,1300,0,0}};
+	for (int ich=0; ich<NOpCh; ich++){
+		printf("\nch%d\n",ich);
+		canvases->Canvas_Decoding->cd(ich+1+1*NOpCh);
+		Na662[ich] = new TF1(Form("Na662_ch%d",ich),"gaus(0)+pol1(3)", fitpar[ich][1]-3*fitpar[ich][2],fitpar[ich][1]+3*fitpar[ich][2]);
+		Na662[ich]->SetParameters(fitpar[ich]);
+		//Na662[ich]->SetParLimits(1,100000,130000);
+		//Na662[ich]->SetParLimits(2,1000,3000);
+		h1_QDC[ich]->Fit(Na662[ich],"M+","",fitpar[ich][1]-3*fitpar[ich][2],fitpar[ich][1]+3*fitpar[ich][2]);
+		h1_QDC[ich]->Fit(Na662[ich],"M+","",Na662[ich]->GetParameter(1)-2*Na662[ich]->GetParameter(2),Na662[ich]->GetParameter(1)+2*Na662[ich]->GetParameter(2));
+	}
+	for (int ich=0; ich<NOpCh; ich++){
+		printf("Eres ch%d=%.2f\n", ich, 235*Na662[ich]->GetParameter(2)/Na662[ich]->GetParameter(1));
+	}
+*/
+	tree->Write();
+	treefile->Write();
+//	canvases->Canvas_Decoding->Write();
+//	for (int i=0; i<3; i++)		canvases->Coincidence[i]->Write();
+			///or, maybe I can make directory in TFile, where I save all histograms
+//	treefile->Close();
+
+	fclose(datafile);
+
+	printf("\n");
+}
---
+++ include/DecoderCanvas.h
@@ -0,0 +1,257 @@
+class DECODER_CANVAS{
+	private:
+		UInt_t NOpCh;
+		UInt_t NOpGr;
+	public:
+		TCanvas** Canvas_Decoding;
+		TCanvas** Coincidence;
+
+		void Construct();
+		void Destruct();
+		void Modified_Update(char flag);
+		void Print(char _filename[90]);
+		//void CanvasWrite(TFile *_treefile);
+
+		DECODER_CANVAS(UInt_t _NOpGr, UInt_t _NOpCh){
+			NOpGr=_NOpGr;
+			NOpCh=_NOpCh;
+			Canvas_Decoding=NULL;
+			Coincidence=NULL;
+		}
+		~DECODER_CANVAS(){
+			Destruct();
+		}
+};
+
+void DECODER_CANVAS::Construct(){
+	Canvas_Decoding = new TCanvas*[NOpGr];
+	for (int igr=0; igr<NOpGr; igr++){
+		Canvas_Decoding[igr] = new TCanvas(Form("Decoding_gr%d",igr),Form("Decoding_gr%d",igr),1800, 1000);
+		Canvas_Decoding[igr]->Divide(NOpCh,3);
+	}
+
+	//Coincidence = new TCanvas*[3];
+	//Coincidence[0] = new TCanvas("QDC_H1_COIN", "QDC_H1_COIN", 900, 954);
+	//Coincidence[1] = new TCanvas("QDC_H2_COIN", "QDC_H1_COIN", 900, 954);
+	//Coincidence[2] = new TCanvas("TDCdif_H1_COIN", "TDCdif_H1_COIN", 900, 954);
+	//for(int i=0; i<3; i++)	Coincidence[i]->Divide(NOpCh,NOpCh);
+
+	return;
+}
+
+void DECODER_CANVAS::Destruct(){
+	for (int igr=0; igr<NOpGr; igr++){	if(!Canvas_Decoding[igr]) delete Canvas_Decoding[igr];	}
+	if(!Canvas_Decoding) delete[] Canvas_Decoding;
+	//if(!Coincidence[0]) delete Coincidence[0];
+	//if(!Coincidence[1]) delete Coincidence[1];
+	//if(!Coincidence[2]) delete Coincidence[2];
+	//if(!Coincidence) delete[] Coincidence;
+	return;
+}
+
+void DECODER_CANVAS::Modified_Update(char flag){
+	for (int ich=0; ich<NOpCh; ich++){
+		if(flag & 0x01){
+			for (int igr=0; igr<NOpGr; igr++){
+				Canvas_Decoding[igr]->cd(1+ich+0*NOpCh); gPad->Modified(); gPad->Update();
+				Canvas_Decoding[igr]->cd(1+ich+1*NOpCh); gPad->Modified(); gPad->Update();
+				Canvas_Decoding[igr]->cd(1+ich+2*NOpCh); gPad->Modified(); gPad->Update();
+			}
+		}
+/*		for (int jch=0; jch<NOpCh; jch++){
+			if(flag & 0x02){ 
+				Coincidence[0]->cd(1+ich+jch*NOpCh); gPad->Modified(); gPad->Update();
+			}
+			if(flag & 0x04){ 
+				Coincidence[1]->cd(1+ich+jch*NOpCh); gPad->Modified(); gPad->Update();
+			}
+			if(flag & 0x08){
+				Coincidence[2]->cd(1+ich+jch*NOpCh); gPad->Modified(); gPad->Update();
+			}
+		}
+*/	}
+	return;
+}
+
+void DECODER_CANVAS::Print(char _filename[90]){
+	for (int igr=0; igr<NOpGr; igr++)	Canvas_Decoding[igr]->Print(Form("jpg/%s_Decoding_c1_gr%d.jpg",_filename, igr));
+//	for (int i=0; i<3; i++)	Coincidence[i]->Print(Form("jpg/%s_Decoding_c%d.jpg",_filename,i+1));
+}
+/*
+void DECODER_CANVAS::CanvasWrite(TFile *_treefile){
+	_treefile->cd();
+	Canvas_Decoding->Write();
+	for (int i=0; i<3; i++){
+	Coincidence[i]->Write();
+	}
+}
+*/
+
+////////////////////////////////////////////
+
+class DECODER_PRIMARY_H1{
+	private:
+		UInt_t NOpCh;
+		UInt_t igr;
+
+	public:
+		TH1D **rPH;
+		TH1D **QDC;
+		TH1D **TDC;
+		void Draw(DECODER_CANVAS *canvas, char flag=0x0F);
+
+		DECODER_PRIMARY_H1(UInt_t _igroup, UInt_t _NOpCh, int QDCrange, int waveform_length, float sampletime){
+			igr=_igroup;
+			NOpCh=_NOpCh;
+			rPH = new TH1D*[NOpCh];		QDC = new TH1D*[NOpCh];	TDC = new TH1D*[NOpCh];
+			for (int ich=0; ich<NOpCh; ich++){
+				rPH[ich] = new TH1D(Form("h1_rPH[%d]_gr%d",ich, igr),Form("h1_rPH[%d]_gr%d;rPH;count/rPH",ich, igr), 4096, 0, 4096);
+				QDC[ich] = new TH1D(Form("h1_QDC[%d]_gr%d",ich, igr),Form("h1_QDC[%d]_gr%d;QDC;count/%dQDC", ich, igr, QDCrange/2000), 2000, 0, QDCrange);
+				TDC[ich] = new TH1D(Form("h1_TDC[%d]_gr%d",ich, igr),Form("h1_TDC[%d]_gr%d;TDC;count/%.1fns",ich, igr, sampletime), waveform_length, 0, waveform_length*sampletime);
+			}
+		}
+		~DECODER_PRIMARY_H1(){
+			for (int ich=0; ich<NOpCh; ich++) delete rPH[ich];
+			for (int ich=0; ich<NOpCh; ich++) delete QDC[ich];
+			for (int ich=0; ich<NOpCh; ich++) delete TDC[ich];
+			delete[] rPH;
+			delete[] QDC;
+			delete[] TDC;
+
+
+		}
+
+};
+inline void DECODER_PRIMARY_H1::Draw(DECODER_CANVAS *canvas, char flag=0x0F){
+	if(!(flag & 0x01)) return;
+	for (int ich=0; ich<NOpCh; ich++){
+		canvas->Canvas_Decoding[igr]->cd(1+ich+0*NOpCh)->SetLogy();
+		canvas->Canvas_Decoding[igr]->cd(1+ich+0*NOpCh)->SetGridx();	rPH[ich]->Draw();
+		canvas->Canvas_Decoding[igr]->cd(1+ich+1*NOpCh)->SetLogy();
+		canvas->Canvas_Decoding[igr]->cd(1+ich+1*NOpCh)->SetGridx();	QDC[ich]->Draw();
+		canvas->Canvas_Decoding[igr]->cd(1+ich+2*NOpCh)->SetLogy();
+		canvas->Canvas_Decoding[igr]->cd(1+ich+2*NOpCh)->SetGridx();	TDC[ich]->Draw();
+	}
+	return;
+}
+//////////////////////////////////////////
+
+class DECODER_QDC_H1_COIN{
+	private:
+		double binsize=1;
+		int nbin=4000;
+		int ich; 
+		UInt_t NOpCh;
+	public:
+		TH1D **coin;
+		void Draw(DECODER_CANVAS *canvas, char flag=0x0F);
+
+
+		DECODER_QDC_H1_COIN(UInt_t _NOpCh, int _ich, int QDCrange){
+			NOpCh=_NOpCh; ich=_ich;
+			binsize=(double)QDCrange/(double)nbin;
+			coin = new TH1D*[NOpCh];
+			for (int jch=0; jch<NOpCh; jch++){
+				coin[jch] = new TH1D(Form("QDC_ch%d_coin%d", ich,jch),Form("QDC_ch%d_coin%d;QDC;count/%.0fQDC", ich,jch, binsize), nbin,0,QDCrange);
+			}
+			
+		}
+		~DECODER_QDC_H1_COIN(){
+			for (int jch=0; jch<NOpCh; jch++) delete coin[jch];
+			delete[] coin;
+		}
+};
+inline void DECODER_QDC_H1_COIN::Draw(DECODER_CANVAS *canvas, char flag=0x0F){
+	if(!(flag & 0x02)) return;
+	for (int jch=0; jch<NOpCh; jch++){
+		canvas->Coincidence[0]->cd(1+ich+jch*NOpCh)->SetLogy();
+		canvas->Coincidence[0]->cd(1+ich+jch*NOpCh)->SetGridx();
+		coin[jch]->Draw();
+	}
+	return;
+}
+//////////////////////////////////////////////
+
+class DECODER_QDC_H2{
+	private:
+		double binsize;
+		int nbin=4000;
+		int ich; 
+		UInt_t NOpCh;
+	public:
+		TH2D **yaxis;
+		void Draw(DECODER_CANVAS *canvas, char flag=0x0F);
+
+		DECODER_QDC_H2(UInt_t _NOpCh, int _ich, int QDCrange){
+			NOpCh=_NOpCh; ich=_ich;
+			binsize=(double)QDCrange/(double)nbin;
+			yaxis = new TH2D*[NOpCh];
+			for (int jch=0; jch<NOpCh; jch++){
+				yaxis[jch] = new TH2D(Form("QDC_xch%d_QDC_ych%d", ich,jch),Form("QDC_xch%d_QDC_ych%d;QDC_ch%d;QDC%d", ich,jch,ich,jch), 4000,0,QDCrange, 4000,0,QDCrange);
+			}
+			
+		}
+		~DECODER_QDC_H2(){
+			for (int jch=0; jch<NOpCh; jch++) delete yaxis[jch];
+			delete[] yaxis;
+		}
+};
+inline void DECODER_QDC_H2::Draw(DECODER_CANVAS *canvas, char flag=0x0F){
+	if(!(flag & 0x04)) return;
+	for (int jch=0; jch<NOpCh; jch++){
+		canvas->Coincidence[1]->cd(1+ich+jch*NOpCh)->SetLogz();
+		canvas->Coincidence[1]->cd(1+ich+jch*NOpCh)->SetGridx();
+		canvas->Coincidence[1]->cd(1+ich+jch*NOpCh)->SetGridy();
+		yaxis[jch]->Draw("COLZ");
+	}
+	return;
+}
+///////////////////////////////////////////////////////
+
+
+class DECODER_TDCDIF_H1{
+	private:
+		double binsize=0.02; // (ns)
+		int nbin;
+		int ich; 
+		UInt_t NOpCh;
+	public:
+		TH1D **startch;
+		void Draw(DECODER_CANVAS *canvas, char flag=0x0F);
+
+		DECODER_TDCDIF_H1(UInt_t _NOpCh, int _ich, double T_lb=-3, double T_ub=3){
+			NOpCh=_NOpCh; ich=_ich;
+			nbin=(int) ((T_ub-T_lb)/binsize);
+			startch = new TH1D*[NOpCh];
+			for (int jch=0; jch<NOpCh; jch++){
+				startch[jch] = new TH1D(Form("TDCdif_stopch%d-startch%d", ich,jch),Form("TDC_stopch%d-startch%d;TDC dif(ns);count/%.3fns", ich,jch, binsize), nbin, T_lb, T_ub);
+			}
+		}
+		~DECODER_TDCDIF_H1(){
+			for (int jch=0; jch<NOpCh; jch++) delete startch[jch];
+
+		}
+};
+inline void DECODER_TDCDIF_H1::Draw(DECODER_CANVAS *canvas, char flag=0x0F){
+	if(!(flag & 0x08)) return;
+	for (int jch=0; jch<NOpCh; jch++){
+		canvas->Coincidence[2]->cd(1+ich+jch*NOpCh)->SetLogy();
+		canvas->Coincidence[2]->cd(1+ich+jch*NOpCh)->SetGridx();
+		startch[jch]->Draw();
+	}
+	return;
+}
+
+
+
+
+///////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
